f463708bf6bd4119fc3d41e9d8c99fceb49a57a9
Benjamin Patton <benjaminpatton@Benjamins-MacBook-Pro.local>
2021-02-14 15:02:45 -0500

    Tried changing analytics to oribi.

--- pages/index.js
+++ pages/index.js
@@ -23,15 +23,11 @@ export default function Blog({
 }) {
   return ( <Layout home>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZJEVFYV5F"></script>
+        <script async type="application/javascript"></script>
           <script
             async
             dangerouslySetInnerHTML={{
-                __html: `window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-LZJEVFYV5F');`
+                __html: `(function(b,o,n,g,s,r,c){if(b[s])return;b[s]={};b[s].scriptToken="XzE1NDM4MjA3NTI";b[s].callsQueue=[];b[s].api=function(){b[s].callsQueue.push(arguments);};r=o.createElement(n);c=o.getElementsByTagName(n)[0];r.async=1;r.src=g;r.id=s+n;c.parentNode.insertBefore(r,c);})(window,document,"script","https://cdn.oribi.io/XzE1NDM4MjA3NTI/oribi.js","ORIBI");`
             }}
           />
         <title>{siteTitle}</title>
